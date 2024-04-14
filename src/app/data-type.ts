1e1f584e9333d60e64352f8b8ec73fb3cbc13a67
Gracy Arora <gracyarora812@gmail.com>
2024-04-14 13:46:09 +0530

    list-components

--- src/app/data-type.ts
+++ src/app/data-type.ts
@@ -11,6 +11,7 @@ export interface login{
     
 }
 export interface product{
+   
     name:string,
     price:number,
     category:string,
--- src/app/seller-home/seller-home.component.ts
+++ src/app/seller-home/seller-home.component.ts
@@ -1,6 +1,8 @@
+import { timeStamp } from 'console';
 import { ProductService } from '../services/product.service';
 import { product } from './../data-type';
 import { Component, OnInit } from '@angular/core';
+import { faTrash} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-seller-home',
@@ -8,13 +10,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './seller-home.component.css',
 })
 export class SellerHomeComponent implements OnInit {
-  productList: undefined | product[]
+  productList: undefined | product[];
+  productMessage: undefined | string;
+  icon = faTrash;
   constructor(private product: ProductService) {}
 
   ngOnInit(): void {
+    this.list();
+    
+  }
+  deleteProduct(id: number) {
+    console.warn('test id', id);
+
+    this.product.deleteProduct(id).subscribe((result) => {
+      if (result) {
+        this.productMessage = 'Product is deleted';
+        this.list();
+      }
+    })
+    setTimeout(()=>{
+      this.productMessage=undefined
+    },3000);
+  }
+  list(){
     this.product.productList().subscribe((result) => {
       console.warn(result);
-      this.productList=result;
+      this.productList = result;
     });
   }
 }
